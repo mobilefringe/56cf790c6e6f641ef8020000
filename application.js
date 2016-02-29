3324fcc6f90b6caa8db7fc42a0adb4cf21f66c72
rajbir@mobilefringe.com @ CodeCloud <rajbir@mobilefringe.com @ CodeCloud>
2016-02-29 12:01:30 -0500

    nustylist 1.0  /application.js

--- application.js
+++ application.js
@@ -10,26 +10,27 @@ $(document).ready(function(){
         custom.description = "New sign up on nuStylist Website";
         custom.email = $('#fieldEmail').val();
         post_data.custom = custom;
-        $.getJSON(
-            this.action + "?callback=?",
-            $(this).serialize(),
-            function (data) {
-                if (data.Status === 400) {
-                    alert("An error occured while processing your request. Please try again later.");
-                } else { // 200
-                    $.post("http://home.mallmaverick.com/custom_email", post_data, function(data, status, xhr){
-                        if(status == "success"){
-                            $("#subscription_confirmed").fadeIn();
-                            $('#subscription_confirmed').delay(2000).fadeOut();
-                            $('#subForm').trigger('reset');
-                            $('.sign_up_btn').prop('disabled', false)
+        $.post("http://home.mallmaverick.com/custom_email", post_data, function(data, status, xhr){
+            if(status == "success"){
+                $("#subscription_confirmed").fadeIn();
+                $('#subscription_confirmed').delay(2000).fadeOut();
+                $('#subForm').trigger('reset');
+                $('.sign_up_btn').prop('disabled', false)
+                $.getJSON(
+                    this.action + "?callback=?",
+                    $(this).serialize(),
+                    function (data) {
+                        if (data.Status === 400) {
+                            alert("An error occured while processing your request. Please try again later.");
+                        } else { // 200
+                            
+                            
                         }
-                        else{
-                            alert("Unable to process your request. Please try again later.")
-                        }
-                    });
-                    
-                }
+                });
+            }
+            else{
+                alert("Unable to process your request. Please try again later.")
+            }
         });
     });
 });
