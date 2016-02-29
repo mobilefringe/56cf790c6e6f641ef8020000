c070d6d5fe66b227b6802d7ebc41dcdf23900a87
rajbir@mobilefringe.com @ CodeCloud <rajbir@mobilefringe.com @ CodeCloud>
2016-02-29 11:31:14 -0500

    nustylist 1.0  /application.js

--- application.js
+++ application.js
@@ -1,32 +1,34 @@
-$('#subForm').submit(function(e){
-    alert("here")
-    e.preventDefault();
-    $('.sign_up_btn').prop('disabled', true)
-    var post_data = {}
-    post_data.mailto = "rajbir@mobilefringe.com";
-    post_data.subject = "nuStylist Sign up";
-    custom = {};
-    custom.email = $('#fieldEmail').val()
-    post_data.custom = custom;
-    $.getJSON(
-        this.action + "?callback=?",
-        $(this).serialize(),
-        function (data) {
-            if (data.Status === 400) {
-                alert("An error occured while processing your request. Please try again later.");
-            } else { // 200
-                $.post("http://home.mallmaverick.com/custom_email", post_data, function(data, status, xhr){
-                    if(status == "success"){
-                        $("#subscription_confirmed").fadeIn();
-                        $('#subscription_confirmed').delay(2000).fadeOut();
-                        $('#subForm').trigger('reset');
-                        $('.sign_up_btn').prop('disabled', false)
-                    }
-                    else{
-                        alert("Unable to process your request. Please try again later.")
-                    }
-                });
-                
-            }
+$(document).ready(function(){
+    $('#subForm').submit(function(e){
+        alert("here")
+        e.preventDefault();
+        $('.sign_up_btn').prop('disabled', true)
+        var post_data = {}
+        post_data.mailto = "rajbir@mobilefringe.com";
+        post_data.subject = "nuStylist Sign up";
+        custom = {};
+        custom.email = $('#fieldEmail').val()
+        post_data.custom = custom;
+        $.getJSON(
+            this.action + "?callback=?",
+            $(this).serialize(),
+            function (data) {
+                if (data.Status === 400) {
+                    alert("An error occured while processing your request. Please try again later.");
+                } else { // 200
+                    $.post("http://home.mallmaverick.com/custom_email", post_data, function(data, status, xhr){
+                        if(status == "success"){
+                            $("#subscription_confirmed").fadeIn();
+                            $('#subscription_confirmed').delay(2000).fadeOut();
+                            $('#subForm').trigger('reset');
+                            $('.sign_up_btn').prop('disabled', false)
+                        }
+                        else{
+                            alert("Unable to process your request. Please try again later.")
+                        }
+                    });
+                    
+                }
+        });
     });
 });
