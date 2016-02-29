1ca86315b824fe1d99b58264d8bdb93c41ab09f1
rajbir@mobilefringe.com @ CodeCloud <rajbir@mobilefringe.com @ CodeCloud>
2016-02-29 12:41:54 -0500

    nustylist 1.0  /application.js

--- application.js
+++ application.js
@@ -23,10 +23,6 @@ $(document).ready(function(){
                     function (data) {
                         if (data.Status === 400) {
                             alert("An error occured while processing your request. Please try again later.");
-                        } else { // 200
-                            $("#subscription_confirmed").fadeIn();
-                            $('#subscription_confirmed').delay(2000).fadeOut();
-                            $('#subForm').trigger('reset');
                         }
                 });
             }
