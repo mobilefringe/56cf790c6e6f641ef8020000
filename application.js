b3e8370abc7b8ec693ff8a0930e9296290394bb6
rajbir@mobilefringe.com @ CodeCloud <rajbir@mobilefringe.com @ CodeCloud>
2016-02-29 11:22:50 -0500

    nustylist 1.0  /application.js

--- application.js
+++ application.js
@@ -8,6 +8,17 @@ $('#subForm').submit(function(e){
             if (data.Status === 400) {
                 alert("An error occured while processing your request. Please try again later.");
             } else { // 200
+                $.post("http://home.mallmaverick.com/custom_email", post_data, function(data, status, xhr){
+                    if(status == "success"){
+                        $("#signup_confirmed").fadeIn();
+                            $('#signup_confirmed').delay(2000).fadeOut();
+                            $('#signup_form').trigger('reset');
+                            $('.send_btn').prop('disabled', false);
+                    }
+                    else{
+                        alert("Unable to process your request. Please try again later.")
+                    }
+                });
                 $("#subscription_confirmed").fadeIn();
                 $('#subscription_confirmed').delay(2000).fadeOut();
                 $('#subForm').trigger('reset');
