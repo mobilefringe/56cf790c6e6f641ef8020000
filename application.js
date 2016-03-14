1a7ae466eb5fee9791b2b96e809a03b26ab938c8
rajbir@mobilefringe.com @ CodeCloud <rajbir@mobilefringe.com @ CodeCloud>
2016-03-14 14:19:06 -0400

    nustylist 1.0  /application.js

--- application.js
+++ application.js
@@ -10,30 +10,34 @@ $(document).ready(function(){
         custom.description = "New sign up on nuStylist Website";
         custom.email = $('#fieldEmail').val();
         custom.name = $('#fieldName').val();
-        
-        
         post_data.custom = custom;
         if($('#first_name').val() !="" && $('#last_name').val() != "" && $('#fieldEmail').val() != ""){
-            $.post("http://home.mallmaverick.com/custom_email", post_data, function(data, status, xhr){
-                if(status == "success"){
-                    
-                    
-                    $.getJSON("http://mobilefringe.createsend.com/t/d/s/krkjku/?callback=?",$('#subForm').serialize(),function (data) {
-                        if (data.Status === 400) {
-                            alert("An error occured while processing your request. Please try again later.");
-                        }
-                        else{
-                           $("#subscription_confirmed").fadeIn();
-                            $('#subscription_confirmed').delay(2000).fadeOut();
-                            $('#subForm').trigger('reset');
-                            $('.sign_up_btn').prop('disabled', false)
-                       }
-                    });
-                }
-                else{
-                    alert("Unable to process your request. Please try again later.")
-                }
-            });
+            if(validateEmail($('#fieldEmail').val())){
+                $.post("http://home.mallmaverick.com/custom_email", post_data, function(data, status, xhr){
+                    if(status == "success"){
+                        
+                        
+                        $.getJSON("http://mobilefringe.createsend.com/t/d/s/krkjku/?callback=?",$('#subForm').serialize(),function (data) {
+                            if (data.Status === 400) {
+                                alert("An error occured while processing your request. Please try again later.");
+                            }
+                            else{
+                               $("#subscription_confirmed").fadeIn();
+                                $('#subscription_confirmed').delay(2000).fadeOut();
+                                $('#subForm').trigger('reset');
+                                $('.sign_up_btn').prop('disabled', false)
+                           }
+                        });
+                    }
+                    else{
+                        alert("Unable to process your request. Please try again later.")
+                    }
+                });
+            }
+            else{
+                alert("Please fill out the required fields.")
+                $('.sign_up_btn').prop('disabled', false)
+            }
             var iOS = /iPad|iPhone|iPod/.test(navigator.userAgent) && !window.MSStream;
             if (iOS){
                 $("#subscription_confirmed").fadeIn();
