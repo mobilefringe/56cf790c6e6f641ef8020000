8bd3973d1102562923e84be7bdef1277ddb60017
rajbir@mobilefringe.com @ CodeCloud <rajbir@mobilefringe.com @ CodeCloud>
2016-03-14 14:17:53 -0400

    nustylist 1.0  /application.js

--- application.js
+++ application.js
@@ -10,6 +10,8 @@ $(document).ready(function(){
         custom.description = "New sign up on nuStylist Website";
         custom.email = $('#fieldEmail').val();
         custom.name = $('#fieldName').val();
+        
+        
         post_data.custom = custom;
         if($('#first_name').val() !="" && $('#last_name').val() != "" && $('#fieldEmail').val() != ""){
             $.post("http://home.mallmaverick.com/custom_email", post_data, function(data, status, xhr){
